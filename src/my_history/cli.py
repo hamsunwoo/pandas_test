9a55b7f8c77151d6ee5774befb8ef0abcb8a1d80
sunwoo <summerham22@gmail.com>
2024-07-24 23:25:10 +0900

    아무것도 입력하지 않았을 때 return값 추가

--- src/my_history/cli.py
+++ src/my_history/cli.py
@@ -13,21 +13,28 @@ def cnt():
     df = pd.read_parquet("~/data/parquet")
     
     args = parser.parse_args()
+
+    if not (args.s or args.t or args.d):
+        print("명령어를 입력해주세요.")
+        return
    
     if args.s:
         fdf = df[df['cmd'].str.contains(args.s)]
         cnt = fdf['cnt'].sum()
-
         print(f'{args.s} 사용 횟수는 {cnt}회 입니다.')
+
+    if args.t and args.d:
+        dt_df = df[df['dt'].str.contains(args.d)]
+        top_dt_df = dt_df.head(args.t).to_string(index=False)
+        print(top_dt_df)
 
     elif args.t:
         top_df = df.head(args.t)
         selected_df = top_df.loc[:, ['cmd','cnt']].to_string(index=False)
-
         print(selected_df)
 
     elif args.d:
         date_df = df[df['dt'].str.contains(args.d)]
         selected_date_df = date_df.loc[:, ['cmd','cnt']].to_string(index=False)
-
         print(selected_date_df)
+
