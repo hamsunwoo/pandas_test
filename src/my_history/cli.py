bea474a89341ad2cc7834890138a537bd573f0ad
sunwoo <summerham22@gmail.com>
2024-07-24 22:29:55 +0900

    clean code

--- src/my_history/cli.py
+++ src/my_history/cli.py
@@ -1,20 +1,17 @@
 import pandas as pd
 import argparse
 
-def arg():
-    parser = argparse.ArgumentParser(description='내가 실행한 명령어 카운트 기능')
 
-    parser.add_argument('-s', help='카운트할 명령어를 입력하세요')
-    parser.add_argument('-t', type=int, help='출력될 데이터 길이를 지정하세요.')
-    parser.add_argument('-d', help='날짜를 입력하세요. YYYY-MM-DD')
+parser = argparse.ArgumentParser(description='내가 실행한 명령어 카운트 기능')
 
-    return parser
+parser.add_argument('-s', help='카운트할 명령어를 입력하세요')
+parser.add_argument('-t', type=int, help='출력될 데이터 길이를 지정하세요.')
+parser.add_argument('-d', help='날짜를 입력하세요. YYYY-MM-DD')
 
 def cnt():
 
     df = pd.read_parquet("~/data/parquet")
     
-    parser = arg()
     args = parser.parse_args()
    
     if args.s:
@@ -26,9 +23,11 @@ def cnt():
     elif args.t:
         top_df = df.head(args.t)
         selected_df = top_df.loc[:, ['cmd','cnt']]
+
         print(selected_df)
 
     elif args.d:
         date_df = df[df['dt'].str.contains(args.d)]
         selected_date_df = date_df.loc[:, ['cmd','cnt']]
-        print(date_df)
+
+        print(selected_date_df)
